79c50286dcb4229aa8778a8b7ce6f75489a2e920
beknazar <beknazar23@gmail.com>
2014-04-30 16:20:41 +0900

    SRM 144 added

---
+++ topcoder/SRM 144/BinaryCode.cpp
@@ -0,0 +1,165 @@
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <sstream>
+#include <typeinfo>
+#include <fstream>
+
+using namespace std;
+
+class BinaryCode {
+private:
+	char elem(const string& str, int index) {
+		if (index >= 0 && index < str.length()) return str[index];
+		return '0';
+	}
+	void check(string& a, const string& message) {
+		int len = message.length();
+		for (int i = 0; i < len - 1; i++) {
+			char c = (message[i]) - (elem(a, i)-'0') - (elem(a, i - 1) - '0');
+			if (c - '0' >= 2) {
+				a = "NONE";
+				return;
+			}
+			else
+			a += c;
+		}
+		if (message[len - 1] != a[len - 1] + (a[len - 2] - '0')) a = "NONE";
+	}
+public:
+	vector<string> decode(string message) {
+		vector<string> s(2);
+		string a = "", b = "";
+		a += '0';
+		check(a, message);
+		b += '1';
+		check(b, message);
+		s[0] = a;
+		s[1] = b;
+		return s;
+	}
+};
+
+// CUT begin
+ifstream data("BinaryCode.sample");
+
+string next_line() {
+	string s;
+	getline(data, s);
+	return s;
+}
+
+template <typename T> void from_stream(T &t) {
+	stringstream ss(next_line());
+	ss >> t;
+}
+
+void from_stream(string &s) {
+	s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+	int len;
+	from_stream(len);
+	ts.clear();
+	for (int i = 0; i < len; ++i) {
+		T t;
+		from_stream(t);
+		ts.push_back(t);
+	}
+}
+
+template <typename T>
+string to_string(T t) {
+	stringstream s;
+	s << t;
+	return s.str();
+}
+
+string to_string(string t) {
+	return "\"" + t + "\"";
+}
+
+template <typename T> string to_string(vector<T> ts) {
+	stringstream s;
+	s << "[ ";
+	for (int i = 0; i < ts.size(); ++i) {
+		if (i > 0) s << ", ";
+		s << to_string(ts[i]);
+	}
+	s << " ]";
+	return s.str();
+}
+
+bool do_test(string message, vector<string> __expected) {
+	time_t startClock = clock();
+	BinaryCode *instance = new BinaryCode();
+	vector<string> __result = instance->decode(message);
+	double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+	delete instance;
+
+	if (__result == __expected) {
+		cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+		return true;
+	}
+	else {
+		cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+		cout << "           Expected: " << to_string(__expected) << endl;
+		cout << "           Received: " << to_string(__result) << endl;
+		return false;
+	}
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+	int cases = 0, passed = 0;
+	while (true) {
+		if (next_line().find("--") != 0)
+			break;
+		string message;
+		from_stream(message);
+		next_line();
+		vector<string> __answer;
+		from_stream(__answer);
+
+		cases++;
+		if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+			continue;
+
+		cout << "  Testcase #" << cases - 1 << " ... ";
+		if ( do_test(message, __answer)) {
+			passed++;
+		}
+	}
+	if (mainProcess) {
+		cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+		int T = time(NULL) - 1398675565;
+		double PT = T / 60.0, TT = 75.0;
+		cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+		cout << "Score  : " << 300 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	cout.setf(ios::fixed, ios::floatfield);
+	cout.precision(2);
+	set<int> cases;
+	bool mainProcess = true;
+	for (int i = 1; i < argc; ++i) {
+		if ( string(argv[i]) == "-") {
+			mainProcess = false;
+		} else {
+			cases.insert(atoi(argv[i]));
+		}
+	}
+	if (mainProcess) {
+		cout << "BinaryCode (300 Points)" << endl << endl;
+	}
+	return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
